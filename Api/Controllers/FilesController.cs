6d277b0cc5226ed105bbfa581d5c08fdb414402e
Usurer <ivvlg@narod.ru>
2023-09-07 18:38:37 +0300

    Different images are shown on the client

--- Api/Controllers/FilesController.cs
+++ Api/Controllers/FilesController.cs
@@ -32,21 +32,29 @@ namespace Api.Controllers
         [HttpGet()]
         public async Task GetBytes()
         {
-            using var image = new FileStream("C:\\Users\\Usurer.000\\Pictures\\DSC06344.JPG", FileMode.Open);
-            var bytes = new byte[image.Length];
-
-            image.Read(bytes);
-
-            var imgSize = BitConverter.GetBytes(image.Length);
-
-            for (int i = 0; i < 10; i++)
+            var files = Directory.EnumerateFiles(@"C:\Users\Usurer.000\Pictures");
+            foreach (var file in files)
             {
-                var writer = new StreamWriter(Response.Body);
-                await Response.Body.WriteAsync(imgSize, 0, 4);
-                await Response.Body.WriteAsync(bytes, 0, bytes.Length);
-                await Response.Body.FlushAsync();
+                var fi = new FileInfo(file);
+                var isImg = fi.Extension.Equals(".jpg", StringComparison.InvariantCultureIgnoreCase)
+                    || fi.Extension.Equals(".jpeg", StringComparison.InvariantCultureIgnoreCase);
 
-                await Task.Delay(1000 * 1);
+                if (isImg)
+                {
+                    using var image = new FileStream(file, FileMode.Open);
+                    var bytes = new byte[image.Length];
+
+                    image.Read(bytes);
+
+                    var imgSize = BitConverter.GetBytes(image.Length);
+
+                    var writer = new StreamWriter(Response.Body);
+                    await Response.Body.WriteAsync(imgSize, 0, 4);
+                    await Response.Body.WriteAsync(bytes, 0, bytes.Length);
+                    await Response.Body.FlushAsync();
+
+                    await Task.Delay(1000 * 1);
+                }
             }
 
             return;
