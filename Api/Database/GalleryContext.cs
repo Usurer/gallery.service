234f194ee5f55b16daa654fcafc0a4ce5cc679ab
Usurer <ivvlg@narod.ru>
2024-01-31 13:01:38 +0300

    Changed DB table

--- Api/Database/GalleryContext.cs
+++ Api/Database/GalleryContext.cs
@@ -13,6 +13,11 @@ public class GalleryContext : DbContext
         get; set;
     }
 
+    public virtual DbSet<ScanTarget> ScanTargets
+    {
+        get; set;
+    }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<FileSystemItem>(entity =>
@@ -28,14 +33,12 @@ public class GalleryContext : DbContext
             entity.Property(e => e.Extension);
         });
 
-        modelBuilder.Entity<FolderScanState>(entity =>
+        modelBuilder.Entity<ScanTarget>(entity =>
         {
-            entity.ToTable("FolderScanStates");
+            entity.ToTable(nameof(ScanTargets));
 
             entity.Property(e => e.Id).HasColumnType("INTEGER").IsRequired();
-            entity.Property(e => e.FileSystemItemId);
             entity.Property(e => e.Path);
-            entity.Property(e => e.LastScanTimestamp);
         });
 
         OnModelCreatingPartial(modelBuilder);
